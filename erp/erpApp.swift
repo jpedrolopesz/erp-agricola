51e1997355861030ccaac47a70c4553c194e6c17
Joao Zamonelo <jplopeszamonelo@hotmail.com>
2026-04-15 20:29:51 -0300

    Initial Commit

---
+++ erp/erpApp.swift
@@ -0,0 +1,32 @@
+//
+//  erpApp.swift
+//  erp
+//
+//  Created by Joao Pedro Lopes Zamonelo on 15/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct erpApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
